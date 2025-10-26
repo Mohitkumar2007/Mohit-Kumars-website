729da152e2ed42a89cd6f46061268538f3084620
Mohit Kumar A <Mohit.a.9459@gmail.com>
2025-10-26 13:36:31 +0000

    add Bootstrap

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+    "id": 1,
+    "title": "Billing-Software"
+}, {
+    "id": 2,
+    "title": "SGPA-Calculator"
+}, {
+    "id": 3,
+    "title": "Downloader"
+}, {
+    "id": 4,
+    "title": "X-Y Calculator"
+}]
+
+
 @app.route('/')
 def hello_Mohit():
-   return render_template('home.html')
+   return render_template('home.html', jobs=JOBS)
+
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0',debug=True)
-
-
+   app.run(host='0.0.0.0', debug=True)
